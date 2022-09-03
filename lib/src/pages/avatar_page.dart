07fb5473423200e0a4d6a8492634a77f4aa142a9
will <wmorales0207@gmail.com>
2022-09-03 16:49:30 +0200

    Cirque avatar  6 vidceo19

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,7 +7,30 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Avatar Page"),
+        title: const Text("Avatar Page1"),
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(2.0),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage('assets/darling.jpg'),
+              radius: 28.0,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
+              backgroundColor: Colors.black54,
+              child: Text('JL'),
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          //image: NetworkImage('uri'),
+          image: AssetImage('assets/download.gif'),
+          placeholder: AssetImage('assets/download.gif'),
+        ),
       ),
     );
   }
