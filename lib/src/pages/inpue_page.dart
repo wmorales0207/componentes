cd85b82f025a02fcf124ae73456f7b8dbd556226
will <wmorales0207@gmail.com>
2022-09-03 19:23:33 +0200

    email pass  6 vidceo22

--- lib/src/pages/inpue_page.dart
+++ lib/src/pages/inpue_page.dart
@@ -9,6 +9,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
+  String _pass = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +22,11 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInputs(),
-          Divider(),
+          const Divider(),
+          _crearEmail(),
+          const Divider(),
+          _crearPassword(),
+          const Divider(),
           _crearPersona(),
         ],
       ),
@@ -54,6 +60,49 @@ class _InputPageState extends State<InputPage> {
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es $_nombre'),
+      subtitle: Text('el Email de la persona es $_email'),
+    );
+  }
+
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'Email@ de la persona',
+        labelText: 'Email',
+        suffixIcon: const Icon(Icons.alternate_email),
+        icon: const Icon(Icons.email),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      onChanged: (value) {
+        // este evento detecta cada una de las teclas que se aprietan pero no repinta el widget para que se repinte se debe usar el SetState.
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Pass de la persona',
+        labelText: 'Pass',
+        suffixIcon: const Icon(Icons.lock),
+        icon: const Icon(Icons.lock_open),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+      onChanged: (value) {
+        // este evento detecta cada una de las teclas que se aprietan pero no repinta el widget para que se repinte se debe usar el SetState.
+        setState(() {
+          _pass = value;
+        });
+      },
     );
   }
 }
