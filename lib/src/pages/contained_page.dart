0f00fe5de6965156a498d224b4e3a4393fee6387
will <wmorales0207@gmail.com>
2022-09-08 11:40:41 +0200

    padding Center del contained

--- lib/src/pages/contained_page.dart
+++ lib/src/pages/contained_page.dart
@@ -13,15 +13,23 @@ class ContainedPage extends StatelessWidget {
         child: Container(
           height: 200.0,
           width: 200.0,
-          
+          padding: EdgeInsets.all(
+              40.0), // este paddin afecta al child dentro dal container.
+
           //color: Colors.lightBlue,// si defines un decoration debes desabilitar esta propiedad
           decoration: BoxDecoration(
             // si defines un deoration debes definir tambienen color inside the decoration
-            borderRadius: BorderRadius.circular(
-                15.0), // miestras mayor es el numero mayor se redondean las puntas, claro es n container cuadtradp
-            color: Colors.deepPurpleAccent,
+            // borderRadius: BorderRadius.circular(
+            //     15.0), // miestras mayor es el numero mayor se redondean las puntas, claro es n container cuadtradp
+            //color: Colors.deepPurpleAccent,
+            //shape: BoxShape.circle,
+            border: Border.all(color: Colors.black12, width: 12.0),
+
             //shape: BoxShape.circle  //aca podemos definir que sea circulo.. y ya el cuadrado se convierte en un circulo
+            borderRadius: BorderRadius.circular(
+                10.0), // un rectangulo con las esquinas ovaladas
           ),
+          child: Center(child: const Text('Testing')),// el center l ppone al centro ignorando el padding.
         ),
       ),
     );
