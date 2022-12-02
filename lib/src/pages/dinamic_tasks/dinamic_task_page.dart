bee78827d12f6af621aef34786d4db6f69b5bfea
Menelik Mesa <menelik.mesa@gmail.com>
2022-12-02 11:44:34 +0100

    adding corrections dinamic tasks

---
+++ lib/src/pages/dinamic_tasks/dinamic_task_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DinamicTaskPage extends StatefulWidget {
+  const DinamicTaskPage({super.key});
+
+  @override
+  State<DinamicTaskPage> createState() => _DinamicTaskPageState();
+}
+
+class _DinamicTaskPageState extends State<DinamicTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
